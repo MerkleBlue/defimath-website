aa0967b5d257abe524b650007d3af7cb7e1cf444
Nebojsa Konsta <nebojsa.konsta@gmail.com>
2026-05-29 21:59:11 +0200

    move icons to the right path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,18 @@ const font = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://defimath.com"),
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+    other: [
+      { rel: "icon", url: "/android-chrome-192x192.png", sizes: "192x192" },
+      { rel: "icon", url: "/android-chrome-512x512.png", sizes: "512x512" },
+    ],
+  },
+  manifest: "/site.webmanifest",
   openGraph: {
     type: "website",
     siteName: "DeFiMath",
